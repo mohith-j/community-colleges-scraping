76f17114e931af9a51cca307499c2138c7c13b42
thompsontr18 <thompsontr18@gmail.com>
2023-06-08 14:36:00 -0400

    updates

---
+++ colleges/southeasterntech.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+def namechange(name):
+	link=name.replace(" - ","-")
+	link = link.replace("- ", "-")
+	link = link.replace("/", "-")
+	link =link.replace(" ", "-")
+	link="https://catalog.southeasterntech.edu/college-catalog/current/courses/"+link
+	r = requests.get(link)
+	if r.status_code != 404:
+		return link
+	else:
+		return None
+
+
+url = "https://catalog.southeasterntech.edu/college-catalog/current/courses"
+html = requests.get(url)
+soup = BeautifulSoup(html.text, "html.parser")
+classy = soup.findAll("div", attrs={"class":"page-title"})
+
+
+li = []
+for classes in classy:
+    x = classes.text.replace("                                                - ", "")
+    x = x.replace("\n", "")
+    mjr = x[0:4]
+    if "GUI" in mjr or "GEN" in mjr or "OGE" in mjr or "OCC" in mjr:
+        continue
+    if mjr not in li:
+        print('---------------------------------')
+        print(mjr)
+        if "L&L" in x:
+            final = x[8:]
+            print(final)
+        else:
+            final = x[9:]
+            if " L" in final[0:2]:
+                final = final[2:]
+            print(final)
+        li.append(mjr)
+    else:
+        final = x[9:]
+        if " L" in final[0:2]:
+            final = final[2:]
+        if final[0] == " ":
+            final = final[1:]
+        if "RNSG" in mjr and final[0] == "B":
+            final = final[1:]
+        print(final)
--- completed colleges/southerncrescenttech.py
+++ completed colleges/southerncrescenttech.py
@@ -6,14 +6,8 @@ def namechange(name):
 	link=name.replace(" - ","-")
 	link = link.replace("- ", "-")
 	link = link.replace("/", "-")
-	link =link.replace(" ", "-")
-	link="https://chattahoocheetech.smartcatalogiq.com/en/2022-2023/General-Catalog/Courses/"+link
-	if "CMTT" in name:
-		link="https://chattahoocheetech.smartcatalogiq.com/2022-2023/General-Catalog/Courses/CMMT"
-	if "DMPT" in name:
-		link = "https://chattahoocheetech.smartcatalogiq.com/2022-2023/General-Catalog/Courses/DMPT-Design-and-Media-Production"
-	if "ELUT" in name:
-		link = "https://chattahoocheetech.smartcatalogiq.com/2022-2023/General-Catalog/Courses/ELUT"
+	link =link.replace(" ", "-").lower()
+	link="https://sctech.smartcatalogiq.com/en/current/catalog/courses/"+link
 	r = requests.get(link)
 	if r.status_code != 404:
 		return link
@@ -21,16 +15,16 @@ def namechange(name):
 		return None
 
 
-url = "https://chattahoocheetech.smartcatalogiq.com/en/2022-2023/General-Catalog/Courses"
+url = "https://sctech.smartcatalogiq.com/en/current/catalog/courses/"
 html = requests.get(url)
 soup = BeautifulSoup(html.text, "html.parser")
 majors = soup.findAll("a", attrs={"class":None}, href=True)
 
 
 for major in majors:
-	if "General Catalog" in major.text:
+	if "General Catalog" in major.text or "Student Calendar" in major.text:
 		continue
-	if "-" in major.text:
+	if len(major.text) == 4:
 		print('---------------------------------')
 		print(major.text)
 		newurl = namechange(major.text)
@@ -38,9 +32,10 @@ for major in majors:
 		newsoup = BeautifulSoup(newhtml.text, "html.parser")
 		classes = newsoup.findAll("a", attrs={"class":None}, href=True)
 		for classy in classes:
-			if "General Catalog" in classy.text:
+			if "General Catalog" in classy.text or "Student Calendar" in classy.text or "100%" in classy.text or "2023-2024" in classy.text:
 				continue
 			if ("0" in classy.text or "1" in classy.text or "2" in classy.text) and len(classy.text) > 5:
 				print(classy.text)
-		if "Welding" in major.text:
+		if "WELD" in major.text:
 			break
+		#okay
