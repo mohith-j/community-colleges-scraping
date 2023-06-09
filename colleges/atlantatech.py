a059f77ffe93c6350dfaf92877fdff2d88392d04
thompsontr18 <thompsontr18@gmail.com>
2023-06-09 09:54:36 -0400

    updates

--- colleges/atlantatech.py
+++ colleges/atlantatech.py
@@ -2,29 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-def namechange(name):
-	link=name.replace(" - ","-")
-	link = link.replace("- ", "-")
-	link = link.replace("/", "-")
-	link =link.replace(" ", "-")
-	link="https://atlantatech.smartcatalogiq.com/en/2022-2023/College-Catalog/Courses/"+link
-	r = requests.get(link)
-	if r.status_code != 404:
-		return link
-	else:
-		return None
 
-
-url = "https://atlantatech.smartcatalogiq.com/en/2022-2023/College-Catalog/Courses"
+url = "https://atlantatech.smartcatalogiq.com/2022-2023/College-Catalog/Courses"
 html = requests.get(url)
 soup = BeautifulSoup(html.text, "html.parser")
-majors = soup.findAll("a", attrs={"class":None}, href=True)
+classy = soup.findAll("a", attrs={"class":None}, href=True)
 
-li = soup.findAll("li", attrs={"class":"hasChildren"})
 
-for major in majors:
-    print(major.text)
-    for l in li:
-        print(l.text)
-    break
+li = []
+count = 0
+for classes in classy:
+	if "ACCT 1100" not in classes.text and count < 1:
+		continue
+	if "ACCT 1100" in classes.text:
+		count += 10
 
+	mjr = classes.text[0:4]
+	if mjr not in li:
+		print('---------------------------------')
+		print(mjr)
+		final = classes.text[10:]
+		print(final)
+		li.append(mjr)
+	else:
+		final = classes.text[10:]
+		if final[0] == " ":
+			final = classes.text[11:]
+		print(final)
