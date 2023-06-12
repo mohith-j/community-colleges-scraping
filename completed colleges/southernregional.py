1dc0fb1e850db74f511b0c4a7a388c56342491f2
thompsontr18 <thompsontr18@gmail.com>
2023-06-12 15:24:55 -0400

    updates

---
+++ completed colleges/southernregional.py
@@ -0,0 +1,40 @@
+import PyPDF2
+import re
+
+def extract(pdfs):
+	with open(pdfs, "rb") as pdf:
+		reader = PyPDF2.PdfReader(pdf, strict=False)
+		text = []
+
+		for page in reader.pages:
+			content = page.extract_text()
+			text.append(content)
+
+		return text
+
+extractedText = extract('southernregional.pdf')
+
+
+pattern = r"[A-Z]{4} \d{4} \- [A-Z].+"
+matches = []
+
+for text in extractedText:
+	match = re.findall(pattern, text)
+	if match:
+		matches.extend(match)
+
+
+li = []
+final = []
+for m in matches:
+	if m[0:9] not in li:
+		li.append(m[0:9])
+		final.append(m)
+final = sorted(final)
+currmjr = ""
+for i in final:
+	if currmjr != i[0:4]:
+		print('---------------------------------')
+		print(i[0:4])
+	currmjr = i[0:4]
+	print(i)
