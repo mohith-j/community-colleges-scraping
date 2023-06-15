b7a79cee5acce10e3080694d1e4f4ec878cf9f21
thompsontr18 <thompsontr18@gmail.com>
2023-06-15 16:04:41 -0400

    moved gupton

--- colleges/guptonJonesColFuneralServices.py
+++ colleges/guptonJonesColFuneralServices.py
@@ -1,5 +1,7 @@
 import PyPDF2
 import re
+import pandas as pd
+from openpyxl import load_workbook
 
 def extract(pdfs):
     with open(pdfs, "rb") as pdf:
@@ -14,7 +16,12 @@ def extract(pdfs):
 #AC-Catalog-2022-2023-08082022
 extractedText = extract('completed colleges/guptonjones.pdf')
 
+book = load_workbook('data.xlsx')
+sheet=book.worksheets[0]
 
+
+
+df = pd.DataFrame(columns=['Colleges','Majors','Courses'])
 pattern = r"[A-Z]{3} [0-9]{3} [A-Z].+\("
 matches = []
 
@@ -27,4 +34,6 @@ for text in extractedText:
 for m in matches:
     m=m[:-1]
     print(m)
-
+    df.loc[len(df.index)] = ["Andrew College",MAJORNAME, m]
+with pd.ExcelWriter('data.xlsx',mode='a', if_sheet_exists='overlay') as writer:  
+    df.to_excel(writer,sheet_name="Sheet",header=False, index=False, startrow=sheet.max_row)
